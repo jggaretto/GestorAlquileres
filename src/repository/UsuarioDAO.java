ca39d5d3fe07bccfc0625a3fd48c0fba09431670
MartinUrtiz <urtizbereamartin19@gmail.com>
2026-06-08 14:12:27 -0300

    Agrego JBCrypt para seguridad del proyecto

--- src/repository/UsuarioDAO.java
+++ src/repository/UsuarioDAO.java
@@ -1,33 +1,35 @@
 package repository;
 
 import model.Usuario;
-import repository.Conexion;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import org.mindrot.jbcrypt.BCrypt;
 
 public class UsuarioDAO {
 
     public Usuario validarLogin(String username, String passwordIngresada) {
         Usuario usuarioValidado = null;
-        
-        // Comparamos password directamente (texto plano)
-        String sql = "SELECT id, username, rol FROM usuarios WHERE username = ? AND password = ?";
+
+        String sql = "SELECT id, username, password, rol FROM usuarios WHERE username = ?";
 
         try (Connection conn = Conexion.getConexion()) {
             if (conn != null) {
                 try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-                    
+
                     stmt.setString(1, username);
-                    stmt.setString(2, passwordIngresada); // Enviamos el texto tal cual
 
                     try (ResultSet rs = stmt.executeQuery()) {
                         if (rs.next()) {
-                            usuarioValidado = new Usuario();
-                            usuarioValidado.setId(rs.getInt("id"));
-                            usuarioValidado.setUsername(rs.getString("username"));
-                            usuarioValidado.setRol(rs.getString("rol"));
+                            String passwordGuardada = rs.getString("password");
+
+                            if (BCrypt.checkpw(passwordIngresada, passwordGuardada)) {
+                                usuarioValidado = new Usuario();
+                                usuarioValidado.setId(rs.getInt("id"));
+                                usuarioValidado.setUsername(rs.getString("username"));
+                                usuarioValidado.setRol(rs.getString("rol"));
+                            }
                         }
                     }
                 }
@@ -35,6 +37,7 @@ public class UsuarioDAO {
         } catch (SQLException e) {
             System.err.println("Error en la consulta: " + e.getMessage());
         }
+
         return usuarioValidado;
     }
 }

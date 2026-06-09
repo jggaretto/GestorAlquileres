bf572b05b3904a124512982fa9cd69b4f4ad91cc
lulaqui15 <quirogalucila15@gmail.com>
2026-06-08 22:01:06 -0300

    Cambios en propietario / controller / PropietarioDAO

--- src/controller/PropietarioController.java
+++ src/controller/PropietarioController.java
@@ -1,5 +1,29 @@
 package controller;
 
+import model.Propietario;
+import repository.PropietarioDAO;
+import java.util.List;
+
 public class PropietarioController {
+
+    private PropietarioDAO repo = new PropietarioDAO();
+
+    public boolean agregar (Propietario p) {
+        return repo.agregar(p);
+    }
+
+    public List<Propietario> listar() {
+        return repo.listar();
+    }
+
+    public boolean actualizar(Propietario p) {
+        return repo.actualizar(p);
+    }
+    public boolean eliminar(int id) {
+        return repo.eliminar(id);
+    }
+    public List<Propietario> buscar(String nombre) {
+        return repo.buscar(nombre);
+    }
     
 }
--- src/model/Propietario.java
+++ src/model/Propietario.java
@@ -1,5 +1,52 @@
 package model;
 
 public class Propietario {
-    
+
+    private int id; 
+    private String nombre;
+    private String apellido; 
+    private String telefono;
+    private String email;
+
+    //constructor vacio (para crear objetos sin datos)
+    public Propietario() {
+    }
+
+    //constructor sin id : para INSERT 
+    public Propietario(String nombre, String apellido, String telefono, String email) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    //constructor con id : para SELECT y UPDATE
+    public Propietario (int id,String nombre, String apellido, String telefono, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    //Getters y Setters
+    public int getId() { return id;}
+    public void setId(int id) { this.id = id; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getApellido() { return apellido; }
+    public void setApellido(String apellido) { this.apellido = apellido; }
+
+    public String getTelefono() { return telefono; }
+    public void setTelefono(String telefono) { this.telefono = telefono; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    @Override
+    public String toString() {
+        return nombre + " " + apellido;
+    }
 }
